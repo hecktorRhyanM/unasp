f5ca8dd1bf6cc48ffe9395645670be52f897b11e
hecktorRhyanM <hecktorrhyanm@gmail.com>
2026-08-19 09:54:02 -0300

    v1.1

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@ Console.WriteLine("Hello, World!");
 int number = 67;
 bool isEasyTeacher = number == 67;
 Console.WriteLine(isEasyTeacher);
+string test = "10";
+int testInt = Convert.ToInt32(test);
+Console.WriteLine(testInt);
+Console.WriteLine(testInt is int);
+Console.WriteLine(test is string);
